d2e41d1086a7e198db2e975f18f26616db3d9759
BilbaoJ <jessicabilbao02@gmail.com>
2023-11-06 12:37:08 -0500

    Configuracion de tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   devServer: {
     port: 3001
-  }
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
